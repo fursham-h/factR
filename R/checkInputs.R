df417d9213f6500b1cabdb06cf31019c64aaad1e
Fursham <37694204+Fursham@users.noreply.github.com>
2019-12-30 06:32:55 +0000

    Improvements to checking function

--- R/checkInputs.R
+++ R/checkInputs.R
@@ -1,6 +1,6 @@
 #' Check input files for workflow
 #'
-#' @param input GRanges object containing imported query GTF data 
+#' @param query GRanges object containing imported query GTF data 
 #' @param ref GRanges object containing reference GTF data
 #' @param fasta BSgenome or Biostrings object containing genomic sequence
 #'
@@ -14,45 +14,63 @@
 #' @importFrom stats dist end start
 #' @importFrom utils head tail
 #' @importFrom rlang .data
-checkInputs <- function(input, ref, fasta) {
+checkInputs <- function(query, ref, fasta) {
+  
+  argnames <- as.character(match.call())[-1]
 
   # check seqlevels
-  message("Report on SeqLevels:")
-  styleI <- GenomeInfoDb::seqlevelsStyle(input)[1]
-  styleR <- GenomeInfoDb::seqlevelsStyle(ref)[1]
-  styleF <- GenomeInfoDb::seqlevelsStyle(fasta)[1]
-  levels <- c(styleI, styleR, styleF)
-  names <- c("input", "ref", "fasta")
-
-  if (length(unique(levels)) == 1) {
-    message("\tSeqlevels on all objects are identical")
+  message("Checking inputs for consistent seqlevels...")
+  
+  seqreport <- unlist(lapply(list(query,ref), function(x){
+    return(sum(!GenomeInfoDb::seqlevels(x) %in% GenomeInfoDb::seqlevels(fasta)))
+  }))
+  
+  if (sum(seqreport) == 0){
+    message('\tOK...')
   } else {
-    lapply(unique(levels), function(x) {
-      objs <- paste(names[which(levels == x)], collapse = ", ")
-      message(sprintf("\t%s objects have Seqlevel style of %s", objs, x))
+    lapply(seq_along(seqreport), function(x) {
+      if (seqreport[x] > 0){
+        message(sprintf("\t%s contain inconsistent seqlevels",
+                        argnames[x]))
+        message(sprintf("\tTry running: %s <- matchSeqLevels(%s, %s)",
+                argnames[x], argnames[x], argnames[3]))
+      }
     })
-    message("\tUse function matchSeqLevels")
   }
 
   # check metadata
-  message("Report on input metadata")
-  inputIDs <- c('gene_id', 'transcript_id') %in% names(mcols(input))
-  refIDs <- c('gene_id', 'transcript_id') %in% names(mcols(ref))
-  
-  
-  # check gene_id
-  message("Report on gene_ids:")
-  inIDs <- unique(input$gene_id)
-  refIDs <- unique(ref$gene_id)
-  unmatched <- sum(!inIDs %in% refIDs)
-  if (unmatched == 0) {
-    message("\tAll query gene_ids matched to reference")
+  message("\nChecking inputs for transcript_id and gene_id attributes...")
+  attreport <- lapply(list(query,ref), function(x){
+    return(!c('gene_id', 'transcript_id') %in% names(S4Vectors::mcols(x)))
+  })
+  if (sum(unlist(attreport)) == 0) {
+    message('\tOK...')
+    # check gene_id
+    message("\nChecking query for mismatched gene_ids...")
+    inIDs <- unique(query$gene_id)
+    refIDs <- unique(ref$gene_id)
+    unmatched <- sum(!inIDs %in% refIDs)
+    if (unmatched == 0) {
+      message("\tOK...")
+    } else {
+      message(sprintf(
+        "\t%s out of %s query gene_ids are not found in reference",
+        unmatched, length(inIDs)
+      ))
+      message(sprintf("\tTry running: %s <- matchGeneIDs(%s, %s)", 
+                      argnames[1], argnames[1], argnames[2]))
+    }
   } else {
-    message(sprintf(
-      "\t%s out of %s query gene_ids are not matched to reference",
-      unmatched, length(inIDs)
-    ))
-    message("\tUse function matchGeneIDs")
+    errorindex <- unlist(lapply(seq_along(attreport), function(x) {
+      if (sum(attreport[[x]]) > 0) {
+        missing <- c('gene_id', 'transcript_id')[attreport[[x]]]
+        message(sprintf("\t%s have missing %s attribute(s)",
+                        argnames[x], paste(missing, collapse = ',')))
+        return(x)
+      }
+    }))
+    
+    message(sprintf('\tPlease check %s object(s) and rerun checkInput',
+                    paste(argnames[errorindex], collapse = ',')))
   }
-  
 }
