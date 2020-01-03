78d8f5345bfd09cb258424565095ae27355e4ba3
“Fursham” <fursham.h@gmail.com>
2020-01-03 15:02:35 +0000

    Added feature to select analysis and returned fields

--- R/extractCDSfeature.R
+++ R/extractCDSfeature.R
@@ -7,6 +7,14 @@
 #' BSgenome or Biostrings object containing genomic sequence
 #' @param which
 #' List containing names of transcripts from cds to filter for analysis
+#' @param hmmer
+#' List of fields to report from hmmer protein domain search. 'default' will
+#' return 'desc', 'evalue' and 'nincluded' columns. See ?bio3d::hmmer for a list
+#' of returned fields. Set argument to NULL to skip hmmer analysis
+#' @param ...
+#' Other features to test and its fields to return. Written as named list with
+#' features as names and list of fields as arguements (eg. signalHsmm = 'default')
+#' Current supported features:
 #'
 #' @return
 #' Dataframe containing protein features for each cds entry
@@ -18,7 +26,8 @@
 #' @examples
 #' library(BSgenome.Mmusculus.UCSC.mm10)
 #' extractCDSfeature(query_cds, Mmusculus)
-extractCDSfeature <- function(cds, fasta, which = NULL) {
+extractCDSfeature <- function(cds, fasta, which = NULL,
+                              hmmer = 'default', ...) {
 
   # catch missing args
   mandargs <- c("cds", "fasta")
@@ -29,6 +38,38 @@ extractCDSfeature <- function(cds, fasta, which = NULL) {
       paste(setdiff(mandargs, passed), collapse = ", ")
     ))
   }
+  
+  # extract features to test as list and check for supported features
+  features <- c(as.list(environment())['hmmer'], list(...))
+  if (any(!names(features) %in% c('hmmer', 'signalHsmm'))) {
+    features_support <- names(features) %in% c('hmmer', 'signalHsmm')
+    warning(sprintf('feature(s) `%s` is not supported and its analysis is not run', 
+                    paste(features[!features_support], collapse = ',')))
+    features <- features[features_support]
+  }
+  
+  # check for correct feature column names for return
+  hmmer.header <- c("name","acc","bias","desc","evalue","flags","hindex","ndom",
+                    "nincluded","nregions","nreported","pvalue","score","taxid",
+                    "pdb.id","bitscore","mlog.evalue")
+  if ('default' %in% features$hmmer) {   # convert default into list of headers
+    features$hmmer <- c('desc', 'evalue', 'nincluded',
+                         features$hmmer[features$hmmer != 'default'] 
+                         ) %>% unique()
+  }
+  if (any(!features$hmmer %in% hmmer.header)){
+    fields_support <- features$hmmer %in% hmmer.header
+    warning(sprintf('`%s` are not supported hmmer reported fields', 
+                    features$hmmer[!fields_support]))
+    if (sum(fields_support) == 0) {
+      features$hmmer <- c('desc', 'evalue', 'nincluded')
+      warning('returning default hmmer fields')
+    } else {
+      features$hmmer <- features$hmmer[fields_support]
+    }
+  }
+  
+  
   # define global variables
   x <- startendaa <- nincluded <- desc <- id <- NULL
 
@@ -81,40 +122,82 @@ extractCDSfeature <- function(cds, fasta, which = NULL) {
     }
     aaSeq <- aaSeqORF
   }
+  
+  # prepare output
+  output <- aaSeq %>% dplyr::select(id)
 
-  # run hmmer function
-  outDomain <- BiocParallel::bplapply(aaSeq$x, function(x) {
+  # run hmmer function if requested
+  if (!is.null(features$hmmer)) {
+    output <- BiocParallel::bplapply(seq_along(aaSeq), function(y) {
+      # account for return errors
+      report <- tryCatch(
+        bio3d::hmmer(aaSeq[y,]$x,
+                     type = "hmmscan",
+                     verbose = F, db = "superfamily"
+        ),
+        error = function(e) NULL
+      )
+      
+      if (is.null(report)) {
+        return("NA")
+      } else {
+        report <- dplyr::bind_cols(id = aaSeq[y,]$id, 
+                                   report$hit.tbl)
+        return(report)
+      }
+    }, BPPARAM = BiocParallel::MulticoreParam()) %>%
+      dplyr::bind_rows()
+    
+    # subset columns based on requested fields
+    output <- output %>% 
+      dplyr::select(id, features$hmmer) %>% 
+      dplyr::rename_at(dplyr::vars(features$hmmer), 
+                       ~ paste0('hmmer.', features$hmmer)) %>%
+      dplyr::right_join(aaSeq %>% dplyr::select(id), by = 'id')
+  }
+  
 
-    # account for return errors
-    report <- tryCatch(
-      bio3d::hmmer(x,
-        type = "hmmscan",
-        verbose = F, db = "superfamily"
-      ),
-      error = function(e) NULL
-    )
-
-    if (is.null(report)) {
-      return("NA")
-    } else {
-      report <- report$hit.tbl %>%
-        dplyr::rowwise() %>%
-        dplyr::mutate(id = paste0(nincluded, "x", desc)) %>%
-        dplyr::ungroup()
-      return(report$id %>% paste(collapse = ";"))
-    }
-  }, BPPARAM = BiocParallel::MulticoreParam())
-
+  
   # run signalHsmm function
-  outSP <- BiocParallel::bplapply(aaSeq$y, function(x) {
-    report <- signalHsmm::run_signalHsmm(x)
-    return(report[[1]]$sp_probability)
-  }, BPPARAM = BiocParallel::MulticoreParam())
-
-  # prepare output df
-  aaSeq <- aaSeq %>%
-    dplyr::select(id) %>%
-    dplyr::bind_cols(list("Domains" = unlist(outDomain))) %>%
-    dplyr::bind_cols(list("SignalP_prob" = unlist(outSP)))
-  return(aaSeq)
+  if (!is.null(features$signalHsmm)) {
+    # check for correct field names for return
+    signalHsmm.header <- c("sp_probability","sp_start","sp_end",
+                        "struc","prot","name","str_approx")
+    if ('default' %in% features$signalHsmm) {   # convert default into list of headers
+      features$signalHsmm <- c("sp_probability",
+                            features$signalHsmm[features$signalHsmm != 'default'] 
+      ) %>% unique()
+    }
+    if (any(!features$signalHsmm %in% signalHsmm.header)){
+      fields_support <- features$signalHsmm %in% signalHsmm.header
+      warning(sprintf('`%s` are not supported signalHsmm reported fields', 
+                      features$signalHsmm[!fields_support]))
+      if (sum(fields_support) == 0) {
+        features$signalHsmm <- "sp_probability"
+        warning('returning default signalHsmm fields')
+      } else {
+        features$signalHsmm <- features$signalHsmm[fields_support]
+      }
+    }
+    
+    # run signalHsmm
+    outSP <- BiocParallel::bplapply(seq_along(aaSeq), function(x) {
+      report <- signalHsmm::run_signalHsmm(aaSeq[x,]$y)[[1]]
+      report$struc <- paste(report$struc, collapse = '')
+      report$prot <- paste(report$prot, collapse = '')
+      
+      report <- report %>% unlist() %>% dplyr::bind_rows() %>%
+        dplyr::mutate(id = aaSeq[x,]$id)
+      return(report)
+    }, BPPARAM = BiocParallel::MulticoreParam()) %>% dplyr::bind_rows()
+    
+    # combine outSP with output
+    output <- outSP %>%
+      dplyr::select(id, features$signalHsmm) %>% 
+      dplyr::rename_at(dplyr::vars(features$signalHsmm), 
+                       ~ paste0('signalHsmm.', features$signalHsmm)) %>%
+      dplyr::left_join(output, ., by = 'id')
+  }
+  
+  return(output)
 }
