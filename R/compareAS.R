ba834ce14d626d5640c362d2dca2421dc0eaf129
“Fursham” <fursham.h@gmail.com>
2020-01-22 11:19:36 +0000

    Added new function to annotateAS

--- R/compareAS.R
+++ R/compareAS.R
@@ -274,3 +274,48 @@ getAS_ <- function(tx1, tx2) {
     GenomicRanges::makeGRangesListFromDataFrame(split.field = "source", keep.extra.columns = T)
   return(disjoint)
 }
+
+
+.annotate <- function(x) {
+  
+  x <- x %>% as.data.frame() %>%
+    dplyr::filter(type == 'exon') %>%
+    dplyr::group_by(transcript_id) %>%
+    dplyr::arrange(start) %>%
+    dplyr::mutate(pos = dplyr::row_number()) %>% 
+    dplyr::mutate(pos = ifelse(pos == 1, 'First', pos)) %>%
+    dplyr::mutate(pos = ifelse(pos == dplyr::n(), 'Last', pos)) %>%
+    dplyr::select(seqnames, start, end, strand, type, gene_id, transcript_id, pos) %>%
+    GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns = T)
+  
+  # get reduced intron boundaries
+  exonsbytx <- S4Vectors::split(x, ~transcript_id)
+  intronsbytx <- GenomicRanges::psetdiff(unlist(range(exonsbytx)), exonsbytx)
+  intronsreduced <- GenomicRanges::reduce(unlist(GenomicRanges::psetdiff(unlist(range(exonsbytx)), exonsbytx)))
+  
+  # get exons that overlap with reduced introns
+  altexons <- IRanges::findOverlapPairs(x, intronsreduced)
+  
+  # annotate AS exons
+  altannotate <- altexons %>% as.data.frame() %>%
+    dplyr::mutate(AStype = 'CE') %>%
+    dplyr::mutate(AStype = ifelse(first.X.start < second.start & first.X.end < second.end & !first.pos %in% c('First','Last'), 'SD', AStype)) %>%
+    dplyr::mutate(AStype = ifelse(first.X.start > second.start & first.X.end > second.end & !first.pos %in% c('First','Last'), 'SA', AStype)) %>%
+    dplyr::mutate(AStype = ifelse(first.X.start < second.start & first.X.end < second.end & first.pos %in% c('First','Last'), 'LE', AStype)) %>%
+    dplyr::mutate(AStype = ifelse(first.X.start > second.start & first.X.end > second.end & first.pos %in% c('First','Last'), 'FE', AStype)) %>%
+    dplyr::mutate(AStype = ifelse(first.X.start < second.start & first.X.end > second.end, 'RI', AStype)) %>%
+    dplyr::mutate(AStype = ifelse(first.X.start > second.start & first.X.end < second.end & first.pos == 'First', 'FE', AStype)) %>%
+    dplyr::mutate(AStype = ifelse(first.X.start > second.start & first.X.end < second.end & first.pos == 'Last', 'LE', AStype)) %>%
+    dplyr::mutate(AStype = ifelse(first.X.strand == '-', chartr("DAFL", "ADLF", AStype), AStype))
+  
+  # get segments that fall within intron and annotate that segment
+  altexons <- GenomicRanges::pintersect(altexons)
+  altexons$type <- 'AS'
+  altexons$AStype <- altannotate$AStype
+  altexons$pos <- altexons$hit <- NULL
+  
+  return(altexons)
+}
+
+
+
