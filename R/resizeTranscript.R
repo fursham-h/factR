3959a41e12db945b83e8f7797d98eb91ee1a16ef
Fursham <37694204+Fursham@users.noreply.github.com>
2020-03-06 11:18:22 +0000

    Vectorize function

--- R/resizeTranscript.R
+++ R/resizeTranscript.R
@@ -22,31 +22,63 @@
 #' resizeTranscript(gr1, 110, 150)
 resizeTranscript <- function(x, start = 0, end = 0) {
 
+  # define global variables
+  width <- tmp.fwdcumsum <- tmp.revcumsum <- tmp.end <- tmp.start <- NULL
+  
+  # check inputs
+  if (is(x, "GRanges")) {
+    type <- "GR"
+    x <- GenomicRanges::GRangesList(x)
+  } else if (is(x, "GRangesList")) {
+    type <- "GRlist"
+  } else {
+    rlang::abort("x is not of class GRanges or GRangesList")
+  }
+  if (length(start) ==1) {
+    start <- rep(start, length(x))
+  } else if(length(start) != length(x)){
+    startlen <- length(start)
+    xlen <- length(x)
+    rlang::abort(sprintf("Length of `start` (%s) is not equal to `x` (%s)", startlen, xlen))
+  }
+  if (length(end) ==1) {
+    end <- rep(end, length(x))
+  } else if(length(end) != length(x)){
+    endlen <- length(end)
+    xlen <- length(x)
+    rlang::abort(sprintf("Length of `end` (%s) is not equal to `x` (%s)", endlen, xlen))
+  }
+  
   # return if appending length is longer than transcript
-  if (sum(BiocGenerics::width(x)) < (start + end)) {
+  if (any(sum(BiocGenerics::width(x)) < (start + end))) {
     rlang::abort("Appending length is larger than size of transcript")
   }
-
-  # define global variable
-  width <- tmp.fwdcumsum <- tmp.revcumsum <- tmp.end <- tmp.start <- NULL
+  
 
   # retrieve strand information
-  strand <- as.character(BiocGenerics::strand(x))[1]
+  strandList <- as.character(S4Vectors::runValue(BiocGenerics::strand(x)))
 
   # subsequently,we will treat granges as forward stranded
   # so if granges is initially on rev strand, we will swap
-  # the length to append from both ends
-  headlength <- ifelse(strand == "-", end, start)
-  taillength <- ifelse(strand == "-", start, end)
-
-
-
+  trim_df <- tibble::tibble(
+    strand = rep(strandList, lengths(x)),
+    start = rep(start, lengths(x)),
+    end = rep(end, lengths(x))
+  )
+  trim_df <- trim_df %>%
+    dplyr::mutate(tmp.headlength = ifelse(strand == "-", end, start)) %>%
+    dplyr::mutate(tmp.taillength = ifelse(strand == "-", start, end)) %>%
+    dplyr::select(-strand,-start, -end)
+  
+  
   x <- x %>%
     as.data.frame() %>%
+    dplyr::bind_cols(trim_df) %>%
+    dplyr::group_by(group) %>%
     dplyr::arrange(start) %>%
     dplyr::mutate(
-      tmp.fwdcumsum = cumsum(width) - headlength,
-      tmp.revcumsum = rev(cumsum(rev(width))) - taillength,
+      tmp.fwdcumsum = cumsum(width) - tmp.headlength,
+      tmp.revcumsum = rev(cumsum(rev(width))) - tmp.taillength,
       tmp.start = start,
       tmp.end = end
     ) %>%
@@ -57,7 +89,17 @@ resizeTranscript <- function(x, start = 0, end = 0) {
     ) %>%
     dplyr::arrange(ifelse(strand == "-", dplyr::desc(start), start)) %>%
     dplyr::select(-dplyr::starts_with("tmp.")) %>%
-    GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns = T)
-
+    dplyr::ungroup()
+  
+  # format output
+  if (type == "GR") {
+    x <- x %>% dplyr::select(-group, -group_name) %>%
+      GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns = T)
+  } else if (type == "GRlist") {
+    x <- x %>% dplyr::select(-group) %>%
+      GenomicRanges::makeGRangesListFromDataFrame(keep.extra.columns = T, 
+                                                  split.field = "group_name")
+  }
+  
   return(x)
 }
