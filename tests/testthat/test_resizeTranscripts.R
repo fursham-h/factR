36f14473f71ec54bead281cde487689456b52ce4
Fursham <37694204+Fursham@users.noreply.github.com>
2020-09-29 14:12:31 +0100

    Update style

--- tests/testthat/test_resizeTranscripts.R
+++ tests/testthat/test_resizeTranscripts.R
@@ -15,7 +15,7 @@ gr2 <- GenomicRanges::GRanges(
     end = c(100, 600, 1100)
   )
 )
-grl <- GenomicRanges::GRangesList(gr1=gr1,gr2=gr2)
+grl <- GenomicRanges::GRangesList(gr1 = gr1, gr2 = gr2)
 
 out1 <- trimTranscripts(gr1, 20, 80)
 out2 <- trimTranscripts(gr2, 20, 80)
@@ -23,7 +23,7 @@ out3 <- trimTranscripts(gr1, 110, 150)
 out4 <- trimTranscripts(gr2, 110, 150)
 
 out5 <- trimTranscripts(grl, 20, 80)
-out6 <- trimTranscripts(grl, c(20,110))
+out6 <- trimTranscripts(grl, c(20, 110))
 
 
 test_that("Test sample output", {
@@ -38,14 +38,22 @@ test_that("Test sample output", {
 
   expect_equal(BiocGenerics::start(out4), 550)
   expect_equal(BiocGenerics::end(out4), 591)
-  
-  expect_equal(as.numeric(BiocGenerics::unlist(BiocGenerics::start(out5))), 
-               c(21, 500, 1000, 1000, 500, 81))
-  expect_equal(as.numeric(BiocGenerics::unlist(BiocGenerics::end(out5))), 
-               c(100, 600, 1020, 1080, 600, 100))
-  
-  expect_equal(as.numeric(BiocGenerics::unlist(BiocGenerics::start(out6))), 
-               c(21, 500, 1000, 500, 1))
-  expect_equal(as.numeric(BiocGenerics::unlist(BiocGenerics::end(out6))), 
-               c(100, 600, 1100, 591, 100))
+
+  expect_equal(
+    as.numeric(BiocGenerics::unlist(BiocGenerics::start(out5))),
+    c(21, 500, 1000, 1000, 500, 81)
+  )
+  expect_equal(
+    as.numeric(BiocGenerics::unlist(BiocGenerics::end(out5))),
+    c(100, 600, 1020, 1080, 600, 100)
+  )
+
+  expect_equal(
+    as.numeric(BiocGenerics::unlist(BiocGenerics::start(out6))),
+    c(21, 500, 1000, 500, 1)
+  )
+  expect_equal(
+    as.numeric(BiocGenerics::unlist(BiocGenerics::end(out6))),
+    c(100, 600, 1100, 591, 100)
+  )
 })
