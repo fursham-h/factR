b828678c029f5cdac889bd8aa6e7b66e9a199f1b
“Fursham” <fursham.h@gmail.com>
2020-12-15 09:59:10 +0000

    Updated function options and tests

--- R/subsettranscripts.R
+++ R/subsettranscripts.R
@@ -1,24 +1,34 @@
 #' Subset GTF GRanges object for de novo transcripts
 #' 
 #' @description
-#' subsetNewTranscripts will compare query and reference GTF GRanges and return
-#' transcripts which are only found in query (de novo).
+#' subsetNewTranscripts will compare query and reference GTF GRanges and by
+#' default, return query transcripts with different exon structures from 
+#' transcripts in reference. In addition, query GTF can be further filtered
+#' by removing transcripts of the same name (by.names = TRUE) and of same CDS info
+#' (by.CDS = TRUE) as reference transcripts
 #' 
 #' 
 #' @param query 
 #' GRanges object containing query GTF data.
 #' @param ref 
 #' GRanges object containing reference GTF data.
-#'
+#' @param by.names
+#' Whether to remove query transcripts with same name as reference transcripts. 
+#' Default: FALSE
+#' @param by.CDS
+#' Whether to remove query transcripts with CDS structure as reference transcripts.
+#' Default: FALSE
+#' 
+#' 
+#' 
 #' @return
-#' GRanges object containing exon coordinates from transcripts which are only
-#' found in query.
+#' Filtered GRanges GTF object
 #' 
 #' @export
 #'
 #' @examples
 #' subsetNewTranscripts(matched_query_gtf, ref_gtf)
-subsetNewTranscripts <- function(query, ref) {
+subsetNewTranscripts <- function(query, ref, by.names = FALSE, by.CDS = FALSE) {
   
   # catch missing args
   mandargs <- c("query", "ref")
@@ -60,8 +70,9 @@ Try running: %s <- matchChromosomes(%s, %s)",
   }
 }
 
-.subsetTranscripts <- function(query, ref){
+.subsetTranscripts <- function(query, ref, by.names, by.CDS){
   
+  # Default filtering, by exon coordinates
   # create exons by transcripts
   query_exons <- S4Vectors::split(query[query$type == "exon"], ~transcript_id)
   ref_exons <- S4Vectors::split(ref[ref$type == "exon"], ~transcript_id)
@@ -72,5 +83,24 @@ Try running: %s <- matchChromosomes(%s, %s)",
   )
   
   # return transcripts that are not found in reference
-  return(query[!query$transcript_id %in% names(query_exons)[!is.na(fulloverlap)]])
+  query <- query[!query$transcript_id %in% names(query_exons)[!is.na(fulloverlap)]]
+  
+  # Optional fitering, by name and CDS coordinates
+  if (by.names) {
+    query <- query[!query$transcript_id %in% ref$transcript_id]
+  }
+  if (by.CDS) {
+    # create exons by transcripts
+    query_CDS <- S4Vectors::split(query[query$type == "CDS"], ~transcript_id)
+    ref_CDS <- S4Vectors::split(ref[ref$type == "CDS"], ~transcript_id)
+    
+    # search for exact query and ref matches
+    fullCDSoverlap <- GenomicRanges::findOverlaps(query_CDS, ref_CDS,
+                                               type = "equal", select = "first")
+    
+    # return transcripts that are not found in reference
+    query <- query[!query$transcript_id %in% names(query_CDS)[!is.na(fullCDSoverlap)]]
+  }
+  
+  return(query)
 }
--- tests/testthat/test_subsettranscripts.R
+++ tests/testthat/test_subsettranscripts.R
@@ -6,8 +6,19 @@ test_that("Test subset robustness", {
 })
 
 test_that("Test .subsetTranscripts", {
-  out <- .subsetTranscripts(matched_query_gtf, ref_gtf)
+  out <- .subsetTranscripts(matched_query_gtf, ref_gtf, F, F)
   expect_equal(is(out, "GRanges"), T)
   expect_equal(length(out), 27)
+  expect_equal(unique(out$transcript_id), c("transcript3", "transcript4"))
+})
+
+test_that("Test .subsetTranscripts by.name", {
+  out <- .subsetTranscripts(matched_query_gtf, matched_query_gtf, T, F)
+  expect_equal(length(out), 0)
+})
+
+test_that("Test .subsetTranscripts by.CDS", {
+  out <- .subsetTranscripts(new_query_gtf, ref_gtf, F, T)
+  expect_equal(length(out), 49)
   expect_equal(unique(out$transcript_id), c("transcript3", "transcript4"))
 })
