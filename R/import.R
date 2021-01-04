bb6720537ab8166d95f5b8aa62b4d5384aff4d2d
“Fursham” <fursham.h@gmail.com>
2021-01-04 16:46:11 +0000

    Added new importFASTA function

--- R/import.R
+++ R/import.R
@@ -20,3 +20,28 @@ importGTF <- function(con) {
     rlang::abort(paste(con, " is not in GTF format"))
   }
 }
+
+
+#' Wrapper to import FASTA file
+#'
+#' @description 
+#' This function is a wrapper to Biostrings::readDNAStringSet() function to import
+#' FASTA genome sequence file and simultaneously convert long chromosome names 
+#' (1 dna:chromosome chromosome:GRCm38:1:1:195471971:1 REF) to short names (1)
+#' 
+#' @param con
+#' Path to FASTA file
+#'
+#' @return
+#' Imported DNAStringSet object
+#'
+#' @author Fursham Hamid
+#' @export
+importFASTA <- function(con) {
+  infile <- tryCatch({Biostrings::readDNAStringSet(con)},
+                     error = function(x){
+                       rlang::abort(paste(con, " not found or not in FASTA format"))}
+                     )
+  names(infile) <- stringr::str_split(seqlevels(infile), " ") %>% purrr::map_chr(`[`, 1) 
+  return(infile)
+}
