993ba83e0dd8934419d4e185e4135171ac424d41
Fursham <37694204+Fursham@users.noreply.github.com>
2021-01-12 16:50:03 +0000

    Draft function (to be stashed)

--- R/NMD2.R
+++ R/NMD2.R
@@ -1,4 +1,4 @@
-identifyNMDexons <- function(x, NMD.result = NULL) {
+identifyNMDexons_old2 <- function(x, NMD.result = NULL) {
     
     # checks etc, will add later
     ## check for x format, check for CDS
@@ -17,4 +17,45 @@ identifyNMDexons <- function(x, NMD.result = NULL) {
     
     # group AS exons by reduced intron
     AS.exons$group <-  GenomicRanges::findOverlaps(AS.exons, intronsreduced, select = "first")
+    
+    # retrieve transcripts containing reduced intron
+    tx.intronsreduced <- GenomicRanges::findOverlaps(intronsreduced, range(exonsbytx))
+    tx.intronsreduced <- as.data.frame(tx.intronsreduced)
+    tx.intronsreduced$transcript_id <- names(exonsbytx)[tx.intronsreduced$subjectHits]
+    
+    # generate non-redundant table of exons and its groupings
+    exons.list <- AS.exons %>% 
+        as.data.frame() %>% 
+        tidyr::unite("ASexon", c(seqnames, start, end, strand, AStype)) %>% 
+        dplyr::select(ASexon, transcript_id, group)
+    
+    # for each AS exon, determine which transcripts splices or skips that exon
+    full.exons.list <- exons.list %>%
+        dplyr::select(ASexon, group) %>% 
+        dplyr::distinct() %>% 
+        dplyr::left_join(tx.intronsreduced[,c(1,3)], 
+                         by = c("group"="queryHits")) %>% 
+        dplyr::left_join(exons.list %>% dplyr::mutate(spliced="spliced"),
+                         by = c("ASexon", "group", "transcript_id")) %>% 
+        tidyr::replace_na(list(spliced="skipped"))
+    
+    # fill NMD information
+    full.exons.NMD.list <- full.exons.list %>% 
+       dplyr::left_join(NMD.result[c("transcript","is_NMD")],
+                         by = c("transcript_id"="transcript"))
+    
+    # testinggg
+    NMD.exons.annotate <- full.exons.NMD.list %>% 
+        dplyr::select(-transcript_id) %>% 
+        dplyr::group_by(ASexon, spliced) %>% 
+        dplyr::mutate(data = list(is_NMD)) %>% 
+        dplyr::select(-is_NMD) %>% 
+        dplyr::distinct() %>% 
+        tidyr::spread(spliced, data) %>% 
+        dplyr::mutate(NMDclass = "no") %>% 
+        dplyr::rowwise() %>% 
+        dplyr::mutate(NMDclass = ifelse(all(spliced), "Poison", NMDclass)) %>% 
+        dplyr::mutate(NMDclass = ifelse(all(skipped), "ORF-maintain", NMDclass)) 
+    
+    return(NMD.exons.annotate)
 }
