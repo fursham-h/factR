0b921ccee463ac6075d2c5c0bea68e5e8e545557
Fursham <37694204+Fursham@users.noreply.github.com>
2021-01-12 16:50:15 +0000

    Concept draft #3

---
+++ R/NMD3.R
@@ -0,0 +1,16 @@
+identifyNMDexons <- function(x, NMD.result = NULL) {
+  
+  # checks etc, will add later
+  ## check for x format, check for CDS
+  ## check for NMD.result, if yes check if all tx have NMD annotation
+  
+  # get reference CDS transcript for each gene
+  
+  # get AS segments between NMD transcript and reference transcript
+  
+  # recreate hypothetical tx by inserting/removing AS segments
+  #### difficult bit
+  
+  # test NMD again
+  
+}
